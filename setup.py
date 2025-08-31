2468ec72067df4b7ea95bed0d7da48b2ca0db350
Mahir Gangone <mahirgangone@gmail.com>
2025-08-30 21:47:01 -0400

    Logging and Exception

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
 
+HYPHEN_E_DOT = '-e .'
 
+def get_requirements(file_path: str) -> List[str]:
+    retquirements = []
+    with open(file_path) as file_obj:
+        retquirements = file_obj.readlines()
+        retquirements = [req.replace('\n', '') for req in retquirements]
+
+        if HYPHEN_E_DOT in retquirements:
+            retquirements.remove(HYPHEN_E_DOT)
+    return retquirements
 
 setup(
     name='mlproject',
@@ -8,5 +19,5 @@ setup(
     author='Mahir Gangone',
     author_email='mahirgangone@gmail.com',
     packages=find_packages(), 
-    install_requires=['pandas', 'numpy', 'seaborn'],
+    install_requires=get_requirements('requirements.txt') 
 )
